4de254a6c2c0cb8e676eaed943b8c12d801c039f
Olaoye Oloyede <Olaoye.oloyede@wintershalldea.com>
2025-09-15 20:59:03 +0200

    list of items to ignore

--- data_source.py
+++ data_source.py
@@ -1,6 +1,24 @@
+import pandas as pd
+import numpy as np
+import requests
+import loguru
+
+
+#import the logger
+logger = loguru.logger()
+
 # this is the file that extracts data from the internet 
-def extract(url):
+def extract(url:str,body:dict):
   """
-  extracts data from the given ur
+  extract financial data using the API
   """
-  pass
+  logger.info("starting the extraction for the Api ")
+
+def transformations(df:pd.DataFrame):
+  """
+  Contains the logic that transform data from the raw phase to the final 
+  
+  """
+
+
+def load_or_wwrite_data()
